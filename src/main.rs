02a715b753795f319eeae3d2a12a99c4ef46e623
Natalia Samoilova <ladymarengo@gmail.com>
2022-01-22 13:11:41 +0200

    Start

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,28 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_system(hello_world)
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
+        .add_startup_system(start)
         .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
+#[derive(Component)]
+struct Snake;
+
+fn start(mut commands: Commands) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: Vec3::new(50.0, 50.0, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::rgb(0.5, 0.5, 1.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Snake);
 }
