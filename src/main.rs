dbdc2a2fe197679cf321b93db517ec7e6c27a998
Natalia Samoilova <ladymarengo@gmail.com>
2022-01-22 13:28:52 +0200

    Chande window size

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
+        .insert_resource(WindowDescriptor {
+            width: 400.,
+            height: 400.,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
         .add_startup_system(start)
         .run();
 }
@@ -12,6 +18,7 @@ fn main() {
 struct Snake;
 
 fn start(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands
         .spawn_bundle(SpriteBundle {
             transform: Transform {
